a42d0af99f5392cd8e7e853c891f1bfcd899987b
Ilya Sokolov <developer.sokolov@gmail.com>
2024-04-29 18:03:00 +0300

    setup logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,11 +1,41 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 	"red-shortener/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
-	fmt.Println(cfg)
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("*** Start Red_byte shortener ***", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
 }
